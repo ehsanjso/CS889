38ebccb002d356d6a74c0dfdf32e7bb54d159f44
Brian <bzimmerman@csumb.edu>
2021-03-08 11:25:30 -0700

    Added prompter directory and started the prompter script.

---
+++ prompter/README.md
@@ -0,0 +1,14 @@
+# CS889 -- Prompter
+## How to use
+
+Use a virtual env:
+```bash
+python -m venv {env_name}
+source {env_name}/bin/activate
+```
+
+Install requirements:
+```bash
+pip install -r requirements.txt
+python -m spacy download en_core_web_sm
+```
---
+++ prompter/get_prompt.py
@@ -0,0 +1,25 @@
+import argparse, sys
+
+from paragraph import extract_keywords
+from sentence import process
+
+input_types=['sentence','paragraph']
+if __name__ == "__main__":
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--type', type=str, default='sentence', help='sentence or paragraph (default: sentence)')
+    parser.add_argument('--text', type=str, help='text to analyze')
+    args=parser.parse_args()
+
+    if args.text == None:
+        raise Exception('No argument \'text\' was passed.')
+
+    if args.type not in input_types:
+        raise Exception('Invalid argument \'type\'. Value should be \'paragraph\' or \'sentence\' (Default: \'sentence\').')
+    
+
+    if args.type == 'sentence':
+        test = process(args.text)
+        print(test)
+    elif args.type == 'paragraph':
+        keywords = extract_keywords(args.text)
+        print(keywords)
