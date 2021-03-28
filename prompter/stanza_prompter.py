e07ef09a331c3a1c857f481521738961d4512edd
889root <brian@spodder.io>
2021-03-28 20:27:37 +0000

    Removed the parse annotator from the nlp pipeline

--- prompter/stanza_prompter.py
+++ prompter/stanza_prompter.py
@@ -9,7 +9,7 @@ import random
 def get_prompts(text, prompt_type, show_all=True, server='localhost'):
     with CoreNLPClient(endpoint=f'http://{server}:9000',
                        start_server=StartServer.DONT_START,
-                       annotators=['coref', 'openie', 'parse']) as client:
+                       annotators=['coref', 'openie']) as client:
         document = client.annotate(text)
         document_info = DocumentInfo(document)
         all_questions = get_all_questions(document_info, prompt_type)
